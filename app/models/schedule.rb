7a42414d5bc17b1ccf238d0fd1e533f65c2a6176
ryo924 <ryo.tominaga24@gmail.com>
2020-11-18 16:51:08 +0900

    バリデーションの設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,15 @@
 class Schedule < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :company
+    validates :person
+    validates :date
+    validates :place
+    validates :purpose
+    validates :relation_id
+  end
+
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
