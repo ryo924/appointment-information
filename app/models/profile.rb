92b2506c88873f8e3f102208d8d67978a93e237d
ryo924 <ryo.tominaga24@gmail.com>
2020-11-19 17:01:13 +0900

    バリデーションの設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,12 @@
 class Profile < ApplicationRecord
 
+  with_options presence: true do
+    validates :customer_last_name
+    validates :customer_first_name
+    validates :customer_occupation
+    validates :position
+  end
+
   belongs_to :schedule
 
 end
